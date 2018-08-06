23ab9cb45715ceb79c70a786e2efd512a06222fb
skjanani <41111202+skjanani@users.noreply.github.com>
2018-08-06 12:05:27 +0530

    Add files via upload

---
+++ Null.java
@@ -0,0 +1,17 @@
+package janani;
+import java.util.Scanner;
+public class Null {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String str;
+System.out.println("enter the string");
+Scanner ja=new Scanner(System.in);
+str=ja.next();
+//System.out.println(str);withsemicolon
+if(System.out.printf(str)==null){ //withoutsemicoloun condition
+	
+}
+	}
+
+}
